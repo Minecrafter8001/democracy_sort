b5e76075f7abd709279ff90aff55958ef14fa625
minecrafter8001 <133237713+Minecrafter8001@users.noreply.github.com>
2025-05-12 12:59:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="democratic_sort",
+    version="0.1.0",
+    description="A democratic voting-based sorting algorithm",
+    author="Minecrafter",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
